8a96974294a39d08489542ffb1c7db1ab10c2bdb
Sean OReilly <soreilly424@gmail.com>
2023-01-19 15:36:05 -0500

    adding app to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ const {
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
