280d095559edf32b221e13c3c2d6064cb20acf0d
Sean OReilly <soreilly424@gmail.com>
2023-01-19 15:28:05 -0500

    adding layout and head

---
+++ app/head.tsx
@@ -0,0 +1,30 @@
+export default function Head() {
+  return (
+    <>
+      <title>Sean O'Reilly | Front-end Developer</title>
+      <meta property="og:title" content="Sean O'Reilly's Personal Site" />
+      <meta name="twitter:title" content="Sean O'Reilly's Personal Site" />
+
+      <meta
+        name="description"
+        content="Sean O'Reilly's Personal Site"
+        key="desc"
+      />
+      <meta property="og:description" content="Sean O'Reilly's Personal Site" />
+      <meta
+        name="twitter:description"
+        content="Sean O'Reilly's Personal Site"
+      />
+
+      <meta property="og:image" content="/images/portfolio.png" />
+      <meta name="twitter:image" content="/images/portfolio.png" />
+
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+      <link rel="canonical" href="https://www.seanoreilly.me" />
+      <meta property="og:url" content="https://www.seanoreilly.me" />
+      <meta name="twitter:url" content="https://www.seanoreilly.me" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
