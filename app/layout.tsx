e1a7515ad8e036bc6e4dc6c9cf325102a38ba425
Sean OReilly <soreilly424@gmail.com>
2023-01-19 15:30:13 -0500

    migrating content from app and document to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
+import clsx from 'clsx'
 import React from 'react'
+import '../styles/globals.css'
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const devMode = process.env.ENVIRONMENT === 'dev'
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={clsx(devMode && 'debug-screens')}>{children}</body>
     </html>
   )
 }
