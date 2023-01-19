d81c2d086e71d05bac51f8284fa5fcf63d7a1e9f
Sean OReilly <soreilly424@gmail.com>
2023-01-19 15:35:06 -0500

    migrating component/layout to app/layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,18 @@
 import clsx from 'clsx'
-import React from 'react'
 import '../styles/globals.css'
+import { useAtom } from 'jotai'
+import { themeAtom } from 'store'
+import React, { useEffect } from 'react'
+import { Inter } from '@next/font/google'
+import { AnimatePresence, motion } from 'framer-motion'
+
+import Header from '@/components/Header'
+import Cursor from '@/components/Cursor'
+import Footer from '@/components/Footer'
+import SidebarMenu from '@/components/SidebarMenu'
+
+const browser = typeof window !== 'undefined'
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
@@ -8,10 +20,54 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   const devMode = process.env.ENVIRONMENT === 'dev'
+  const [theme] = useAtom(themeAtom)
+
+  useEffect(() => {
+    console.log(
+      '\n',
+      'Hello there!',
+      '\n',
+      'Want to reach out?!',
+      '\n',
+      'Shoot me an email @',
+      '\n',
+      'soreilly424@gmail.com',
+      '\n',
+      '🤗'
+    )
+  }, [])
+
+  const spring = {
+    type: 'spring',
+    stiffness: 700,
+    damping: 30,
+  }
 
   return (
     <html lang="en">
-      <body className={clsx(devMode && 'debug-screens')}>{children}</body>
+      <body className={clsx(devMode && 'debug-screens')}>
+        <AnimatePresence initial={false}>
+          {theme && (
+            <motion.div
+              transition={spring}
+              animate={{
+                backgroundColor:
+                  theme === 'dark' ? 'var(--dark)' : 'var(--light)',
+              }}
+              className={clsx(
+                'flex min-h-screen flex-col transition-all duration-100 ease-linear dark:bg-opacity-25',
+                inter.className
+              )}
+            >
+              {browser && <Cursor />}
+              <Header />
+              <SidebarMenu />
+              <main className="flex-1 py-20">{children}</main>
+              <Footer />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </body>
     </html>
   )
 }
